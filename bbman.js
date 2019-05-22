30c5a30a71e8e3df77e0d23aae405ecd4ae25de8
Kacper Kaluzynski <kapsel845@wp.pl>
2019-05-22 15:36:38 +0200

    [BBMAN-7] coos tam dodalem

--- bbman.js
+++ bbman.js
@@ -1,8 +1,17 @@
+const Topbar_height=75;
+const Topbar_width=750;
+
 class Topbar{
     constructor(){
         this.model = document.createElement('canvas');
-
+        this.model.id = "topbar";
+        this.model.width = Topbar_width;
+        this.model.height = Topbar_height;
+        this.ctx = this.model.getContext('2d');
+        this.ctx.fillStyle = '#b4b18d';
+        this.ctx.fillRect(0,0,Topbar_height,Topbar_width);
     }
+
 }
 
 class Bomberman {
