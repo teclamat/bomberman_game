d9ea88b3d6cb463f67aa9f6365d0fc204678ffbc
Szymon Zieliński <zielin123@op.pl>
2019-05-30 15:29:50 +0200

    [BBMAN-6] Board it's over

--- bbman.js
+++ bbman.js
@@ -1,10 +1,21 @@
 class Bomberman {
-
-    render(){
-
+    constructor() {
+        this.game = document.createElement('canvas');
+        this.game.width = 750;
+        this.game.height = 650;
+        this.ctx = this.game.getContext('2d');
+        this.ctx.fillStyle = "#393";
+    }
+    render() {
+        this.ctx.fillRect(0, 0, 750, 650);
     }
 
-    build(){
-        
+    init() {
+        document.body.appendChild(this.game);
+        this.render();
     }
 }
+
+const bomberman = new Bomberman();
+
+window.addEventListener('DOMContentLoaded', () => bomberman.init());
