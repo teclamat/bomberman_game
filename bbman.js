757bd69e1302cebcec0caad1f90db50783e6083d
sk44207 <48093262+sk44207@users.noreply.github.com>
2019-05-30 16:08:51 +0200

    [BBMAN-2/1] create Player constructor

--- bbman.js
+++ bbman.js
@@ -11,24 +11,44 @@ class Topbar {
         this.ctx.fillStyle = 'orange';
         this.ctx.fillRect(0, 0, Topbar_width, Topbar_height);
 
-        this.clock(80,40);
+        this.clock(80, 40);
 
 
     }
 
 
 
-    clock(w,h){
+    clock(w, h) {
         let x = 3;
         this.ctx.fillStyle = 'black';
-        this.ctx.fillRect((Topbar_width/2)-(w/2),(Topbar_height/2)-(h/2),w,h);
+        this.ctx.fillRect((Topbar_width / 2) - (w / 2), (Topbar_height / 2) - (h / 2), w, h);
         this.ctx.font = '20px serif';
-        this.ctx.fillText(x,(Topbar_width/2)-(w/2) ,(Topbar_height/2)-(h/2) );
+        this.ctx.fillText(x, (Topbar_width / 2) - (w / 2), (Topbar_height / 2) - (h / 2));
     }
     draw(ctx) {
         ctx.drawImage(this.model, 0, 0);
     }
 }
+
+class Player {
+    constructor(ctx) {
+        this.model = document.createElement('canvas');
+        this.model.width = 36;
+        this.model.height = 50;
+        this.ctx = this.model.getContext('2d');
+        this.ctx.fillRect(0, 0, 36, 50);
+        this.gameCtx = ctx;
+    }
+    render() {
+        this.gameCtx.drawImage(this.model, 10, 10);
+
+    }
+
+    AI() {
+
+    }
+};
+
 
 class Bomberman {
     constructor() {
@@ -37,9 +57,11 @@ class Bomberman {
         this.game.height = 650;
         this.ctx = this.game.getContext('2d');
         this.topbar = new Topbar();
+        this.player = new Player(this.ctx);
     }
     render() {
         this.topbar.draw(this.ctx);
+        this.player.render();
     }
 
     init() {
